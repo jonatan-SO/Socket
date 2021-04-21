6a49614bef5fa136c5b06b5a4b2bc260d73853db
Jonatan Santamaria Olea <jonitanqwe@gmail.com>
2021-04-20 23:58:36 -0500

    subiendo los primero archivos

---
+++ server/dist/classes/server.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const environment_1 = require("../global/environment");
+const http_1 = __importDefault(require("http"));
+class Server {
+    constructor() {
+        this.app = express_1.default();
+        this.port = environment_1.SERVER_PORT;
+        this.httpServer = new http_1.default.Server(this.app);
+        this.io = require("socket.io")(this.httpServer, {
+            cors: {
+                origin: true,
+                credentials: true
+            },
+        });
+        this.escucharSockets();
+    }
+    static get instance() {
+        return this._instance || (this._instance = new this);
+    }
+    escucharSockets() {
+        console.log('Escuchando conexiones - sockets');
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado');
+        });
+    }
+    start(callback) {
+        this.app.listen(this.port, callback());
+    }
+}
+exports.default = Server;
